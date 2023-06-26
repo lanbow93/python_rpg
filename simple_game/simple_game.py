f95d08680fec5fb305971b48d8fda26b3265d357
lanbow93 <the.doctor1501@gmail.com>
2023-06-26 08:35:43 -0500

    Purchase validation able to access info

--- simple_game/simple_game.py
+++ simple_game/simple_game.py
@@ -15,9 +15,10 @@ monster_proper_nouns = ["Drakonis", "Morbos", "Zephyrion", "Nyxar", "Xalos", "Ve
 shop_weapons = {"warrior": ["mace", 10, "broadsword", 20], "wizard": ["grimoire", 10, "staff", 20], "rouge": ["dagger", 10, "poisoned dagger", 20]}
 shop_armor = {"warrior": ["metal plating", 10, "diamond armor", 20], "wizard": ["apprentice robe", 10, "master robe", 20], "rouge":["veil of mystery", 10, "reaper's robe", 20]}
 
-def attempt_weapon_purchase(item, user):
-    print("attempt purchase reached")
-
+def attempt_weapon_purchase(selection_index, user):
+    weapon = shop_weapons[user.being_class][selection_index]
+    price = shop_weapons[user.being_class][selection_index + 1]
+    print(f"{weapon} costs ${price}")
 def generate_enemy(user):
     monster_name = monster_proper_nouns[random.randint(0, len(monster_proper_nouns))]
     if(user.get_experience() < 10):
@@ -37,6 +38,7 @@ def fight(user):
     else:
         gameplay(user)
 
+
 def shop(user):
     os.system("clear")
     print("Shop\n1. Weapon\n2. Armor\n3. Potion\n4. Exit")
@@ -51,7 +53,7 @@ def shop(user):
             list_count += 1
             weapon_selection.append(shop_weapons[user.being_class][i])
         print(weapon_selection)
-        attempt_weapon_purchase(weapon_selection[int(input("\nEnter Selection: "))], user)
+        attempt_weapon_purchase(int(input("\nEnter Selection: "))-1, user)
     elif (user_selection == "2"):
         print("Armor:")
     elif (user_selection == "3"):
