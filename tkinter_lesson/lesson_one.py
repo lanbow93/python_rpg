e74b2bf6bde80f1fc74da28fba7abf4d68916989
lanbow93 <the.doctor1501@gmail.com>
2023-06-29 13:06:57 -0500

    Hello work tkinter program completed

---
+++ tkinter_lesson/lesson_one.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+#Creating root window
+root = tk.Tk()
+
+#Creating a Label widget
+myLabel = tk.Label(root, text="Hello World")
+#Pushing ity onto the screen
+myLabel.pack()
+
+#Launching the program loop
+root.mainloop()
+
